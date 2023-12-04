e2a09e13b8f83763719a5bc32f9cf11bb123e115
Riccardo Andreazza <riccardo.andreazza90@gmail.com>
2023-12-04 12:39:34 +0100

    add: header top

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -8,14 +8,65 @@ export default {
 
 <template>
     <header>
-        <h2>HEADER TOP</h2>
+
+        <div class="header-top">
+            <div class="container">
+                <div class="hour">
+                    <p>
+                        <i class="fa-solid fa-clock"></i>Open Hours: Mon - Sat - 9:00 - 18:00
+                    </p>
+                </div>
+                <div class="contacts">
+                    <p>
+                        <i class="fa-solid fa-phone"></i> + 1 (305) 1234-5678
+                    </p>
+                    <p>
+                        <i class="fa-solid fa-envelope"></i> hello@example.com
+                    </p>
+                    <p>
+                        <i class="fa-brands fa-facebook-f"></i>
+                    </p>
+                    <p>
+                        <i class="fa-brands fa-twitter"></i>
+                    </p>
+                    <p>
+                        <i class="fa-brands fa-linkedin-in"></i>
+                    </p>
+                </div>
+            </div>
+        </div>
         <h2>
             HEADER BOTTOM (JUMBO)
         </h2>
+
     </header>
 </template>
 
 <style lang="scss" scoped>
 @use "../style/partials/mixins" as *;
 @use "../style/partials/variables" as *;
+
+    .header-top {
+        background-color: $header-bg;
+        padding: .5rem;
+        color: $adv-bg;
+        height: 50px;
+        @include flex;
+
+        .container {
+            @include flex (row, space-between);
+
+            .hour {
+                i {
+                    margin-right: .5rem
+                }
+            }
+            .contacts {
+                @include flex;
+                p {
+                    margin-left: 1.5rem;
+                }
+            }
+        }
+    }
 </style>
